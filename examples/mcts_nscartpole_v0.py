01d107d3de8f61d8bae3e3c7ef19d407a6840918
trietdang5599 <trietdang05051999@gmail.com>
2025-10-02 12:09:47 +0800

    modify training

--- examples/mcts_nscartpole_v0.py
+++ examples/mcts_nscartpole_v0.py
@@ -1,22 +1,30 @@
 import gym
-import dyna_gym.agents.mcts as mcts
 
-### Parameters
+from dyna_gym.agents import mcts
+
+# Parameters
+# Add render_mode='human' to visualize if the environment supports it.
 env = gym.make('NSCartPole-v0')
-agent = mcts.MCTS(action_space=env.action_space)
+agent = mcts.MCTS(action_space=env.action_space, rollouts=100)
 timesteps = 100
 verbose = False
 
-### Run
-env.reset()
+# Run episode
+obs, info = env.reset()
 done = False
-for ts in range(timesteps):
-    __, __, done, __ = env.step(agent.act(env,done))
-    if verbose:
+
+for ts in range(1, timesteps + 1):
+    action = agent.act(env, done)
+    obs, reward, terminated, truncated, info = env.step(action)
+    done = terminated or truncated
+
+    if verbose and hasattr(env, 'print_state'):
         env.print_state()
-    env.render()
-    if ts+1 == timesteps:
-        print("Successfully reached end of episode ({} timesteps)".format(ts+1))
+
     if done:
-        print("Episode finished after {} timesteps".format(ts+1))
+        print(f"Episode finished after {ts} timesteps")
         break
+else:
+    print(f"Successfully reached end of episode ({timesteps} timesteps)")
+
+env.close()
